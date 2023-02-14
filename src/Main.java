418db635948288fd6f480ecedd22e75fb874ab03
user <katya.rotar02@gmail.com>
2023-02-14 22:35:48 +0200

    exercise1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -27,36 +28,42 @@ public class Main {
 
     private static void exercise1(Scanner i) {
         System.out.println("Введіть рік, місяць та день народження першої людини");
-        int year1 = i.nextInt();
-        int month1 = i.nextInt();
-        int day1 = i.nextInt();
+        int[] people1 = new int[3];
+        int[] people2 = new int[3];
+        for(int p1 = 0; p1 < people1.length; p1++){
+            people1[p1] = i.nextInt();
+        }
         System.out.println("Введіть рік, місяць та день народження другої людини");
-        int year2 = i.nextInt();
-        int month2 = i.nextInt();
-        int day2 = i.nextInt();
+        for(int p2 = 0; p2 < people2.length; p2++){
+            people2[p2] = i.nextInt();
+        }
+        int[] senior;
+        int[] less;
         int people = 0;
-        int senior;
-        int less;
-        if (year1 < year2) {
-            senior = year1;
-            less = year2;
+        if (people1[0] < people2[0]) {
+             senior = Arrays.copyOf(people1,3);
+             less = Arrays.copyOf(people2,3);
         }
         else {
-            senior = year2;
-            less = year1;
+            senior = Arrays.copyOf(people2,3);
+            less = Arrays.copyOf(people1,3);
         }
-            for ( int a = senior; a <= less; a++){
-                if( a == senior){
-                    people += 12 - month1;
+            for ( int a = senior[0]; a <= less[0]; a++){
+                if( a == senior[0]){
+                    people += 12 - senior[1];
                 }
-                else if(a == year2){
-                    people += month2;
+                else if(a == less[0]){
+                    people += less[1];
                 }
                 else {
                     people += 12;
+                    if(senior[2] > less[2]){
+                        people--;
+                    }
                 }
             }
-        System.out.println(people);
+            String s = Arrays.toString(senior);
+        System.out.println("Старший той хто народився: "+ s +" на "+ people +" повних місяців");
     }
 
     private static void exercise2(Scanner i) {

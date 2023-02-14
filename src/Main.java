0ef4e1f15ac998f65e51ca6d722fb9be7b8ba174
user <katya.rotar02@gmail.com>
2023-02-14 22:11:25 +0200

    exercise1.attempt1

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 
 public class Main {
@@ -25,6 +26,37 @@ public class Main {
     }
 
     private static void exercise1(Scanner i) {
+        System.out.println("Введіть рік, місяць та день народження першої людини");
+        int year1 = i.nextInt();
+        int month1 = i.nextInt();
+        int day1 = i.nextInt();
+        System.out.println("Введіть рік, місяць та день народження другої людини");
+        int year2 = i.nextInt();
+        int month2 = i.nextInt();
+        int day2 = i.nextInt();
+        int people = 0;
+        int senior;
+        int less;
+        if (year1 < year2) {
+            senior = year1;
+            less = year2;
+        }
+        else {
+            senior = year2;
+            less = year1;
+        }
+            for ( int a = senior; a <= less; a++){
+                if( a == senior){
+                    people += 12 - month1;
+                }
+                else if(a == year2){
+                    people += month2;
+                }
+                else {
+                    people += 12;
+                }
+            }
+        System.out.println(people);
     }
 
     private static void exercise2(Scanner i) {

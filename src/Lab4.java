0badb180da186040c89212efaa4189ac8aab0201
user <katya.rotar02@gmail.com>
2023-02-15 11:47:58 +0200

    Lab4

--- src/Lab4.java
+++ src/Lab4.java
@@ -1,8 +1,7 @@
 
-import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main {
+public class Lab4 {
     public static void main(String[] args) {
         Scanner i = new Scanner(System.in);
         while (true) {
@@ -13,7 +12,7 @@ public class Main {
             int start = i.nextInt();
             switch (start) {
                 case 0 -> {}
-                case 1 -> exercise1(i);
+                case 1 -> exercise6(i);
                 case 2 -> exercise3(i);
                 case 3 -> exercise4();
                 default -> System.out.println("Такої дії не існує");
@@ -24,7 +23,45 @@ public class Main {
         }
     }
 
-    private static void exercise1(Scanner i) {
+private static void exercise6(Scanner i){
+    System.out.print("""
+                Арифметичні дії:
+                1 – додавання
+                2 – віднімання
+                3 - множення
+                4 – ділення
+                Введіть номер арифметичної дії
+                """);
+    int number = i.nextInt();
+    System.out.println("Введіть два дійсні числа");
+    int A = i.nextInt();
+    int B = i.nextInt();
+    switch (number){
+        case 1 -> {
+            int action1 = A + B;
+            System.out.println(action1);
+        }
+        case 2 -> {
+            int action2 = A - B;
+            System.out.println(action2);
+        }
+        case 3 -> {
+            int action3 = A * B;
+            System.out.println(action3);
+        }
+        case 4 -> {
+            if (B == 0){
+                System.out.println("На 0 ділити не можна");
+            }
+            else {
+                int action4 = A / B;
+                System.out.println(action4);
+            }
+        }
+        default -> System.out.println("Такої дії не існує");
+    }
+}
+    /*private static void exercise1(Scanner i) {
         System.out.println("Введіть рік, місяць та день народження першої людини");
         int[] people1 = new int[3];
         int[] people2 = new int[3];
@@ -41,6 +78,16 @@ public class Main {
         if (people1[0] < people2[0]) {
              senior = Arrays.copyOf(people1,3);
              less = Arrays.copyOf(people2,3);
+        }
+        if(people1[0] == people2[0]){
+            if(people1[1] < people2[1]){
+                senior = Arrays.copyOf(people1,3);
+                less = Arrays.copyOf(people2,3);
+            }
+            else {
+                senior = Arrays.copyOf(people2,3);
+                less = Arrays.copyOf(people1,3);
+            }
         }
         else {
             senior = Arrays.copyOf(people2,3);
@@ -63,6 +110,8 @@ public class Main {
             String s = Arrays.toString(senior);
         System.out.println("Старший той хто народився: "+ s +" на "+ people +" повних місяців");
     }
+
+     */
 
     private static void exercise3(Scanner i) {
         int[] n = new int[]{3,7,9};
